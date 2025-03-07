f8fe808c530b1c9a4bd22cd2da06873870faccfa
Jose2929 <Jose_2929@live.com.mx>
2025-03-07 00:27:32 -0600

    Bautizo sofia
    Proyecto de bautizo

--- assets/config-ini.js
+++
@@ -1,3 +0,0 @@
-var deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {e.preventDefault();deferredPrompt = e;});
-window.addEventListener('appinstalled', () => { $('#espera').hide(); $('#termino').show(); deferredPrompt = null; });
